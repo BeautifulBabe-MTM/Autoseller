a95c1cbc8e0feb608c09e0c3988439309ba43af1
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2022-12-24 22:30:22 +0200

    Delete routes directory

--- routes/carRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const carController = require('../contollers/carController')
-const checkRole = require('../middleware/checkRoleMiddleware')
-
-router.post('/', checkRole('ADMIN'), carController.create)
-
-module.exports = router
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const carRouter = require('./carRouter')
-const userRouter = require('./userRouter')
-
-router.use('/user', userRouter)
-router.use('/car', carRouter)
-
-module.exports = router

f30d2ecda066114e2fd3a3c4616efd4c03255fc0
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2022-12-25 23:11:45 +0200

    almost dine

---
+++ client/src/components/MainComponent.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
+
+function CarFetch(props) {
+    const [car, setCar] = React.useState([]);
+
+    React.useEffect(() => {
+        getCar();
+    }, [])
+
+    const getCar = async () => {
+        await fetch('/getcars')
+            .then(cursor => cursor.json())
+            .then(cursor => setCar(cursor.cursor));
+    }
+    if (!car) {
+        return <div>
+            <div>Loading...</div>
+        </div>
+    }
+    else if (car == null || car?.length <= 0) {
+        return <div>
+            <div>There is no Cars</div>
+            <div onClick={getCar} className={'button'}>Update</div>
+        </div>
+    }
+
+    return <div className='flex_content'>
+        <div style={{backgroundColor:"red"}}>
+            {car?.map((cursor) => {
+                return <div>
+                    <h2>{cursor.Brand}</h2>
+                </div>
+            })}
+        </div>
+    </div>
+
+};
+
+export default CarFetch;
